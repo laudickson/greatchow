63a8f230ee878e013354353793abd885422f3893
dickson <kr.d.tsl@gmail.com>
2016-06-15 11:52:26 -0400

    initialize topbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :admins
   root to: "homes#index"
 
-  resources :about, only:[:index]
-  resources :menu, only:[:index]
-  resources :events, only:[:index]
-  resources :contact, only:[:index]
+  resources :about, only: :index
+  resources :menu, only: :index
+  resources :events, only: :index
+  resources :contact, only: :index
+  resources :portals, only: :index
 
 end
