33c0b6933ac8f1e8896a896c87b8679b487d3e8d
Joseph Cho <jscho13@gmail.com>
2016-06-27 23:12:48 -0400

    fix form centering and allow emails in development

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   resources :abouts, only: :index
   resources :menus, only: :index
   resources :events, only: :index
-  resources :contacts, only: :index
+  resources :contacts, only: [:index, :new, :create]
   resources :rewards, only: :index
   resources :portals, only: :index
 
   match '/contacts', to: 'contacts#new', via: 'get', :as => :contact
-  resources 'contacts', only: [:new, :create]
 
   authenticate :admin do
     resources :events, only: [:new, :create, :update, :edit, :destroy]
