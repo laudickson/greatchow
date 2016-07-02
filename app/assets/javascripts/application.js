13dc218f825dfa260dfb1de6e9ad3a4ea3a61179
Joseph Cho <jscho13@gmail.com>
2016-07-01 23:15:10 -0400

    fade in content only

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,16 @@ $(window).resize(function() {
 $(function(){
   $('#event-table').tablesorter({ sortList: [[1,0]] });
 });
+
+// below function causes "fade out"
+// http://stackoverflow.com/questions/19397515/fading-out-a-whole-page-with-jquery
+$(document).on("click", "a", function () {
+    var newUrl = $(this).attr("href");
+    if (!newUrl || newUrl[0] === "#") {
+        location.hash = newUrl;
+        return;
+    }
+    $("#content_wrapper").fadeOut(function () {
+        location = newUrl;
+    });
+});
