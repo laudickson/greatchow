f8a3dc597877f59a8153d84815a546a0c45f1646
dickson <kr.d.tsl@gmail.com>
2016-07-21 18:38:45 -0400

    finish homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,47 +14,73 @@
 //= require jquery_ujs
 //= require foundation
 //= require jquery.slick
-//= require initialize
 //= require jquery-tablesorter
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
 
+//--- Tablesorter for portal ---//
 $(function(){
   $('#event-table').tablesorter({ sortList: [[1,0]] });
 });
-
-// $(window).resize(function() {
-//   sideNav();
-// });
-
-// below function causes "fade in" nav triggered on scroll
-
-// below function causes "fade out"
-// https://css-tricks.com/forums/topic/fade-out-between-pages/page/2/
 $(document).ready(function() {
+  //--- Foundation initializiation ---//
+
+  //--- Slick carousel ---//
+  $('.image-slide').not('.slick-initialized').slick({
+      autoplay: true,
+      autoplaySpeed: 2100,
+      fade: true,
+      infinite: true,
+      speed: 900,
+      prevArrow: false,
+      nextArrow: false
+  });
+
+  //--- Fade Out sidenav on scroll ---//
   $(window).scroll(function(){
     if ($(this).scrollTop()>500){
-      $('#navSidebar').css('visibility', 'visible');
-      $('#navSidebar').fadeIn(800);
+      $('#navSidebar').fadeIn(500);
+      $('#navSidebar').css('display', 'flex');
     } else{
       $('#navSidebar').fadeOut(800);
     }
   });
 
+  //--- Parallaxes ---//
+  var $window = $(window);
+  $('section[data-type="background"]').each(function(){
+       var $bgobj = $(this); // assigning the object
+
+       $(window).scroll(function() {
+           var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
+
+           // Put together our final background position
+           var coords = '50% '+ yPos + 'px';
+
+           // Move the background
+           $bgobj.css({ backgroundPosition: coords });
+       });
+   });
+
+  //--- Fade Out content ---//
   $('#content_wrapper').css('visibility', 'visible').hide();
   $('#content_wrapper').fadeIn(1000);
-});
 
-// below function causes "fade out"
-// http://stackoverflow.com/questions/19397515/fading-out-a-whole-page-with-jquery
-$(document).on("click", "a", function () {
-  var newUrl = $(this).attr("href");
-  if (!newUrl || newUrl[0] === "#") {
+  // below function causes "fade out"
+  // http://stackoverflow.com/questions/19397515/fading-out-a-whole-page-with-jquery
+  $(document).on("click", "a", function () {
+    var newUrl = $(this).attr("href");
+    if (!newUrl || newUrl[0] === "#") {
       location.hash = newUrl;
       return;
-  }
-  $("#content_wrapper").fadeOut(function () {
-    location = newUrl;
+    }
+    $("#content_wrapper").fadeOut(function () {
+      location = newUrl;
+    });
   });
+
+  //--- Home page stuff ---///
+  $('#homeLogo').delay(600).fadeIn(800);
+  $('#homeCaption').delay(1000).fadeIn('slow');
 });
